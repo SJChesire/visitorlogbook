db73c6d21c648a6323dd6c8c8c2c654785fff803
shillachesire24 <1046024@cuea.edu>
2022-06-28 11:48:47 +0300

    add admin

--- records/admin.py
+++ records/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+# Register your models here.
+from . import models
+admin.site.register(models.Visitor)
+admin.site.register(models.Visitee)
+admin.site.register(models.Organization)
