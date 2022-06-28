7fcc71defc70896c203b3f555259046b748ca6e1
shillachesire24 <1046024@cuea.edu>
2022-06-28 11:26:39 +0300

    add models

--- records/models.py
+++ records/models.py
@@ -2,4 +2,36 @@ from django.db import models
 
 # Create your models here.
 
+class Visitee(models.Model):
+    visitee_name = models.CharField (max_length = 60)
+    visitee_status = models.CharField(max_length = 4)
+    visitor_name = models.CharField(max_length= 60)
+
+
+
+    def __str__(self):
+        return self.visitee_name
+
+
+class Organization(models.Model):
+    organization_name = models.CharField(max_length=60)
+    mobile_number  = models.IntegerField()
+
+
+    def __str__(self):
+        return self.organization_name
+
+class Visitor(models.Model):
+    visitor_name = models.CharField (max_length = 60)
+    visitee = models.ForeignKey(Visitee, on_delete=models.CASCADE,null=True,blank=True)
+    organization = models.ForeignKey (Organization,  on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=12)
+    id_or_passport = models.CharField (max_length = 20)
+    time_in = models.TimeField()
+    time_out = models.TimeField()
+
+
+    def __str__(self):
+        return self.id_or_passport
+
 
