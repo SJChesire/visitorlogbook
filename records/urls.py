636e8e509b7ca79f2ed48c7942c176bb6e0ae37d
shillachesire24 <1046024@cuea.edu>
2022-06-30 18:23:57 +0300

    add urls

--- records/urls.py
+++ records/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('create_visitor/', views.create_visitor, name='create_visitor'),
+    # path('', views.index, name='index'),
+    path('', views.create_visitor, name='create_visitor'),
+    path('create_visitee/', views.create_visitee, name='create_visitee'),
+    path('create_organization/', views.create_organization, name='create_organization'),
 ]
